1b4014023cb8dc81bdd32e76f4b6811efc2b6a66
Thitphavanh <thitphavanh.psv@gmail.com>
2022-05-07 12:14:00 +0700

    update

--- client.py
+++ client.py
@@ -1,1 +1,18 @@
-client
+import socket
+
+serverip = '192.168.0.54'
+port = 8888
+buffsize = 4096
+
+
+for i in range(10):
+    server = socket.socket()
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.connect((serverip, port))
+
+    data = input('Send to server :')
+    server.send(data.encode('utf-8'))
+
+    data_server = server.recv(buffsize).decode('utf-8')
+    print('Data from server :', data_server)
+    server.close()
--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 
 serverip = '192.168.0.54'
 port = 8888
+buffsize = 4096
 
 server = socket.socket()
-
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server.bind((serverip,port))
 
-server.listen(1)
+while True:
+    server.bind((serverip, port))
+    server.listen(1)
+    print('waiting client...')
+
+    client, add = server.accept()
+    print('connected from :', add)
+
+    data = client.recv(buffsize).decode('utf-8')
+    print('Data from client :', data)
+    client.send('received your message.')
+    client.close()
